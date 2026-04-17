3df55988f79bff87f9e5a636a72e99450dc32a1b
Lucas Daniel <lucasdaniel.csm2020@gmail.com>
2026-04-17 10:06:20 -0300

    completando a Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+import java.time.LocalDateTime;
+import model.Pessoa;
+import model.Consulta;
+import model.Paciente;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Insira o nome do paciente: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Insira a consulta: ");
+        String texto = sc.nextLine();
+
+        String[] consultaArray = { texto };
+
+        Consulta consulta = new Consulta(1, consultaArray, LocalDateTime.now());
+
+        Paciente paciente = new Paciente(nome, 1, consulta);
+        paciente.setNome(nome);
+
+        System.out.println("Paciente " + paciente.getNome() + " cadastrado com sucesso!");
+        System.out.println(LocalDateTime.now());
+        
+        System.out.println("O paciente passará pela: "+paciente.getConsulta().getConsulta()[0]);
+        
+        sc.close();
     }
 }
