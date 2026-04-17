cd48c3307f80c4864cb027bbb7f85354999a2c79
Carlos <k95924376@gmail.com>
2026-04-16 23:31:06 -0300

    Atribuição de métodos às classes 'Consulta' e 'Paciente'

--- src/model/Consulta.java
+++ src/model/Consulta.java
@@ -4,38 +4,58 @@ import java.time.LocalDateTime;
 
 public class Consulta {
     private int idConsulta;
-    private String[] consulta;
+    private String[] consulta; // [0]=sintomas, [1]=observações, [2]=receita
     private LocalDateTime dataConsulta;
-    private Medico idMedico;
-    private Paciente idPaciente;
+    private Paciente paciente;
+    private Medico medico;
 
-    public Consulta(int idConsulta, String[] consulta, LocalDateTime dataConsulta) {
+    public Consulta(int idConsulta, int tamanhoArray, LocalDateTime dataConsulta) {
         this.idConsulta = idConsulta;
-        this.consulta = consulta;
+        this.consulta = new String[tamanhoArray];
         this.dataConsulta = dataConsulta;
     }
 
+    // GETTERS
     public int getIdConsulta() {
         return idConsulta;
-    }
-
-    public void setIdConsulta(int idConsulta) {
-        this.idConsulta = idConsulta;
-    }
-
-    public String[] getConsulta() {
-        return consulta;
-    }
-
-    public void setConsulta(String[] consulta) {
-        this.consulta = consulta;
     }
 
     public LocalDateTime getDataConsulta() {
         return dataConsulta;
     }
 
-    public void setDataConsulta(LocalDateTime dataConsulta) {
-        this.dataConsulta = dataConsulta;
+    public String[] getConsulta() {
+        return consulta;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    // SETTERS CONTROLADOS
+    protected void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    protected void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+
+
+    // Preencher dados fixos da consulta
+    public void setDados(String sintomas, String observacoes, String receita) {
+        if (consulta.length > 0) consulta[0] = sintomas;
+        if (consulta.length > 1) consulta[1] = observacoes;
+        if (consulta.length > 2) consulta[2] = receita;
+    }
+
+    // Verifica se está totalmente agendada
+    public boolean estaAgendada() {
+        return paciente != null && medico != null;
     }
 }
--- src/model/Paciente.java
+++ src/model/Paciente.java
@@ -1,28 +1,65 @@
 package model;
 
 public class Paciente extends Pessoa {
+
     private int idPaciente;
     private Consulta consulta;
 
-    public Paciente(String nome, int idPaciente, Consulta consulta) {
+    public Paciente(String nome, int idPaciente) {
         super(nome);
         this.idPaciente = idPaciente;
-        this.consulta = consulta;
     }
 
+    // GETTERS
     public int getIdPaciente() {
         return idPaciente;
-    }
-
-    public void setIdPaciente(int idPaciente) {
-        this.idPaciente = idPaciente;
     }
 
     public Consulta getConsulta() {
         return consulta;
     }
 
-    public void setConsulta(Consulta consulta) {
+
+    // Agendar consulta com médico
+    public boolean agendarConsulta(Consulta consulta, Medico medico) {
+
+        if (this.consulta != null) {
+            System.out.println("Paciente já possui consulta!");
+            return false;
+        }
+
+        if (consulta.estaAgendada()) {
+            System.out.println("Consulta já está ocupada!");
+            return false;
+        }
+
         this.consulta = consulta;
+
+        consulta.setPaciente(this);
+        consulta.setMedico(medico);
+
+        medico.adicionarConsulta(consulta);
+
+        return true;
+    }
+
+    // Cancelar consulta
+    public void cancelarConsulta() {
+        if (consulta != null) {
+            consulta.setPaciente(null);
+            consulta.setMedico(null);
+            this.consulta = null;
+        }
+    }
+
+    // Ver consulta
+    public void verConsulta() {
+        if (consulta != null) {
+            System.out.println("Paciente: " + getNome());
+            System.out.println("Consulta ID: " + consulta.getIdConsulta());
+            System.out.println("Data: " + consulta.getDataConsulta());
+        } else {
+            System.out.println("Nenhuma consulta agendada.");
+        }
     }
 }
